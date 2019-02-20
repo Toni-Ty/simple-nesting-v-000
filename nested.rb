0fb8e15afa39295a76b86866cfa863f369dd0ce4
Toni-Ty <antonique2479@yahoo.com>
2019-02-20 04:41:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -134,14 +134,4 @@ programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 programmer_hash
 
 end
-contacts = {
-  "Jon Snow" => {
-    name: "Jon",
-    email: "jon_snow@thewall.we",
-    favorite_ice_cream_flavors: ["chocolate", "vanilla"]  },
-  "Freddy Mercury" => {
-    name: "Freddy",
-    email: "freddy@mercury.com",
-    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-  }
-}
+
